440b713cf1aad432a5bfb8a87fee7aa3a493c528
marmot0814 <s1357andy871207@gmail.com>
2019-09-29 23:49:47 +0800

    lab1 3.2. fibonacci serial

--- lab1/fib/main.s
+++ lab1/fib/main.s
@@ -3,11 +3,38 @@
     .thumb
 .text
 .global main
-.equ AA, 0x55AA
+.equ N, 10
+
+fib:
+    // check if 1 > N || 100 < N
+    movs R4, -1
+    movs R1, 1
+    cmp R1, R0
+    bgt end
+    movs R1, 100
+    cmp R1, R1
+    blt end
+    // start fib
+    movs R1, 1  // i
+    movs R2, 1  // b
+    movs R4, 1  // a
+loop:
+    cmp R1, R0
+    bge end
+    movs R3, R2
+    add R2, R2, R4
+    movs R4, R3
+    // overflow
+    cmp R4, 0
+    blt overflow
+    add R1, R1, 1
+    b loop
+overflow:
+    movs R4, -2
+    b end
+end:
+    bx lr
 
 main:
-    movs r0,#(AA&0xFF)
-    orrs r0, r0,#(AA&0xFF00)
-    movs r1,#20
-    adds r2,r0,r1
-    b main
+    movs R0, #N
+    bl fib
