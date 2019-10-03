2193d2662ef724bb5efb5e0e4fea5e2e55032a97
marmot0814 <s1357andy871207@gmail.com>
2019-10-04 00:11:08 +0800

    lab2 gcd

--- lab2/gcd/main.s
+++ lab2/gcd/main.s
@@ -2,17 +2,40 @@
     .cpu cortex-m4
     .thumb
 .text
+    m: .word 918
+    n: .word 1323
 .global main
-.equ AA, 0x55AA
+
+//  int gcd(int a, int b) {
+//      if (b == 0) return a;
+//      return gcd(b, a % b);
+//  }
+
+gcd:
+    push {r0, r1, lr}
+    cmp r1, #0
+    bne cntn
+    ldr r1, =result
+    str r0, [r1]
+    pop  {r0, r1, pc}
+cntn:
+    sdiv r2, r0, r1
+    mul r2, r2, r1
+    sub r2, r0, r2
+    mov r0, r1
+    mov r1, r2
+    bl gcd
+    pop  {r0, r1, pc}
 
 main:
-    movs r0,#(AA&0xFF)
-    orrs r0, r0,#(AA&0xFF00)
-    movs r1,#20
-    adds r2,r0,r1
+    ldr R0, =m
+    ldr R0, [R0]
+    ldr R1, =n
+    ldr R1, [R1]
+    bl gcd
     b main
 
 .bss
 .data
-    .global A
-    A: .word 0xAA
+    result: .word 0
+    max_size:   .word 0
