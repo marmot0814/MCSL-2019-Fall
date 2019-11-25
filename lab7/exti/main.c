455674e79e2bf6a41a8c5f62785530ff9b6a8e1b
Austin Chang <austin880625@gmail.com>
2019-11-25 20:27:41 +0800

    Add lab7

---
+++ lab7/exti/main.c
@@ -0,0 +1,34 @@
+#include "inc/stm32l476xx.h"
+
+// use this pragma at handlers
+//#pragma thumb
+
+void GPIO_init() {
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN;
+
+	GPIOA->MODER = (GPIOA->MODER & 0xFFFFFFFC) | 0x02;
+	GPIOA->AFR[0] = (GPIOA->AFR[0] & 0xFFFFFFF0) | 0x01;
+
+	GPIOB->MODER = (GPIOB->MODER & 0xFFF0003F) | 0x540;
+	GPIOB->OSPEEDR = 0xA80;
+	GPIOB->PUPDR = 0xAA100;
+
+	GPIOC->MODER = (GPIOC->MODER & 0xFFFC03FF) | 0x15400;
+	GPIOC->OSPEEDR = 0x2A800;
+}
+
+int col = 0;
+
+int main() {
+	GPIO_init();
+	while(1) {
+		for(int i=5; i<9; i++) {
+			// mask EXTI on line 9-6
+			// locked
+			col = i;
+			GPIOC->ODR = (1<<i);
+			// unmask EXTI again
+		}
+	}
+	return 0;
+}
